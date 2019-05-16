35638d3828ea02b1a1a7c1e8a2a0b5af68158ce6
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-05-16 17:31:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
 class Student < User
+  attr_accessor :knowledge
+  
+  def initialize(knowledge)
+    @knowledge = []
+  end 
+  
+  def learn(string)
+    @kowledge << string 
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 
 end
